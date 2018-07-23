6266db37ae91b88c3195e16a642b0b3e9d3aa165
Hafidz Azhar <hafidz@formulatrix.com>
2018-07-23 22:41:13 +0700

    remove sub project - PO

--- PO/POProject.BussinessLogic/Entity/MyTable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace POProject.BusinessLogic.Entity
-{
-    public class MyTable
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- PO/POProject.Membership/Entity/MembershipRole.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace POProject.Membership.Entity
-{
-    public class MembershipRole
-    {
-        public string Id_Role { get; set; }
-        public string Name_Role { get; set; }
-        public string Pass_Phrase { get; set; }
-    }
-}
